2a5f2bb7910506ba184577f478c5d5b68931610a
Stefan Müller <s.mueller@it.kls-glt.de>
2016-06-18 11:12:09 +0200

    Clean up examples, the are reimplemented if the safe interface is ok

--- examples/unit-test-client.rs
+++ examples/unit-test-client.rs
@@ -1,20 +1,4 @@
-#[allow(non_camel_case_types)]
-
-
-enum Backend {
-    TCP,
-    TCP_RI,
-    RTU,
-}
-
-struct UnitTestClient;
-
-impl UnitTestClient {
-    fn test_server(&self, backend: Backend) {}
-}
-
 
 
 fn main() {
-    assert!(true);
 }
