64e8ac065d2f032fced15c3c87d9f6eca971aa41
Stefan Müller <s.mueller@it.kls-glt.de>
2017-01-16 14:55:44 +0100

    Fix bug from last commit

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,4 @@ mod error;
 
 // Public reexport, http://rust-lang.github.io/book/chYY-YY-public-api.html
 pub use self::modbus::{Error, Modbus};
-pub use self::modbus::MODBUS_RTU_RTS_DOWN;
-
-// These republished constants are for the doc tests. Because there is no simple way to access raw:: values
-pub const MODBUS_RTU_RS485: c_int = raw::MODBUS_RTU_RS485;
-pub const MODBUS_RTU_RTS_DOWN: c_int = raw::MODBUS_RTU_RTS_DOWN;
+pub use self::raw::{MODBUS_RTU_RTS_DOWN, MODBUS_RTU_RS485};
