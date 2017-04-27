74cafd142ac2a26b70a2d42800a3c3c7e2e1ee0a
Stefan Müller <s.mueller@it.kls-glt.de>
2017-04-27 13:29:45 +0200

    First, useless yet, build.rs version

--- libmodbus-sys/build.rs
+++ libmodbus-sys/build.rs
@@ -1,10 +1,46 @@
 extern crate pkg_config;
 
+use std::env;
+use std::fs::{self, File};
+use std::io::prelude::*;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 
+
+macro_rules! t {
+    ($e:expr) => (match $e{
+        Ok(e) => e,
+        Err(e) => panic!("{} failed with {}", stringify!($e), e),
+    })
+}
 
 fn main() {
     let has_pkgconfig = Command::new("pkg-config").output().is_ok();
 
-    println!("Rock n Roll");
+    if pkg_config::find_library("libmodbus").is_ok() {
+        return
+    }
+
+    if !Path::new("libmodbus/.git").exists() {
+        let _ = Command::new("git").args(&["submodule", "update", "--init"])
+                                   .status();
+    }
+
+    let target = env::var("TARGET").unwrap();
+    let host = env::var("HOST").unwrap();
+    let windows = target.contains("windows");
+    let msvc = target.contains("msvc");
+
+    // Clean up OUTPUT dir
+    let _ = fs::remove_dir_all(env::var("OUT_DIR").unwrap());
+    t!(fs::create_dir_all(env::var("OUT_DIR").unwrap()));
+
+
+    if target.contains("windows") {
+        return
+    }
+
+    if target.contains("apple") {
+        return
+    }
 }
