d5d1c0265f1391b229689d7b46fef15377e4d7a0
Stefan Müller <s.mueller@it.kls-glt.de>
2017-05-10 14:46:55 +0200

    Test som more configure parameter for building under yocto

--- libmodbus-sys/build.rs
+++ libmodbus-sys/build.rs
@@ -1,16 +1,47 @@
 extern crate bindgen;
 extern crate pkg_config;
+extern crate gcc;
 
 use std::env;
+use std::ffi::OsString;
 use std::fs;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
+
+// stolen from: https://github.com/alexcrichton/backtrace-rs/blob/master/backtrace-sys/build.rs
 macro_rules! t {
     ($e:expr) => (match $e{
         Ok(e) => e,
         Err(e) => panic!("{} failed with {}", stringify!($e), e),
     })
+}
+
+// stolen from: https://github.com/alexcrichton/backtrace-rs/blob/master/backtrace-sys/build.rs
+fn try_tool(compiler: &gcc::Tool, cc: &str, compiler_suffix: &str, tool_suffix: &str)
+            -> Option<PathBuf> {
+    if !cc.ends_with(compiler_suffix) {
+        return None
+    }
+    let cc = cc.replace(compiler_suffix, tool_suffix);
+    let candidate = compiler.path().parent().unwrap().join(cc);
+    if Command::new(&candidate).output().is_ok() {
+        Some(candidate)
+    } else {
+        None
+    }
+}
+// stolen from: https://github.com/alexcrichton/backtrace-rs/blob/master/backtrace-sys/build.rs, too
+fn find_tool(compiler: &gcc::Tool, cc: &str, tool: &str) -> PathBuf {
+    // Allow overrides via env var
+    if let Some(s) = env::var_os(tool.to_uppercase()) {
+        return s.into()
+    }
+    let tool_suffix = format!("-{}", tool);
+    try_tool(compiler, cc, "-gcc", &tool_suffix)
+        .or_else(|| try_tool(compiler, cc, "-clang", &tool_suffix))
+        .or_else(|| try_tool(compiler, cc, "-cc", &tool_suffix))
+        .unwrap_or_else(|| PathBuf::from(tool))
 }
 
 const LIBMODBUS_DIR: &'static str = "libmodbus";
@@ -48,6 +79,17 @@ fn main() {
     let _ = fs::remove_dir_all(env::var("OUT_DIR").unwrap());
     t!(fs::create_dir_all(env::var("OUT_DIR").unwrap()));
 
+    let cfg = gcc::Config::new();
+    let compiler = cfg.get_compiler();
+    let cc = compiler.path().file_name().unwrap().to_str().unwrap();
+    let mut flags = OsString::new();
+    for (i, flag) in compiler.args().iter().enumerate() {
+        if i > 0 {
+            flags.push(" ");
+        }
+        flags.push(flag);
+    }
+
     // Generate configure, run configure, make, make install
     run_command("Generating configure",
         Command::new("autoreconf")
@@ -59,7 +101,13 @@ fn main() {
     run_command("Configuring libmodbus",
         Command::new("./configure")
             .arg("--prefix")
-            .arg(prefix)
+            .arg(&prefix)
+            .env("CC", compiler.path())
+            .env("CFLAGS", flags)
+            .arg("--with-pic")
+            .arg("--disable-multilib")
+            .arg("--disable-shared")
+            .arg("--disable-host-shared")
             .arg(format!("--target={}", target))
             .arg(format!("--host={}", host))
             .current_dir(&build_dir));
