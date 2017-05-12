997d242084ff97ea75f37586ef666d6bc9b1cbe0
Stefan Müller <s.mueller@it.kls-glt.de>
2017-05-12 10:54:02 +0200

    Update docs for db8a3ef716579fb8509cd253315283607ec26bff
    Merge branch 'development'

--- implementors/rand/distributions/trait.IndependentSample.js
+++ implementors/rand/distributions/trait.IndependentSample.js
@@ -1,6 +1,7 @@
 (function() {var implementors = {};
 implementors["error_chain"] = [];
 implementors["libc"] = [];
+implementors["rand"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
