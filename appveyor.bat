f78d2b2b08755d7bdb7526027c41593ca1c0d0c1
Stefan Müller zzeroo <co@zzeroo.com>
2020-11-04 12:33:19 +0100

    Update Appveyor

--- appveyor.bat
+++ appveyor.bat
@@ -12,21 +12,14 @@ rem     channel:    stable, beta, nightly
 rem This script based on the work of the libmypaint team.
 
 rem Download rust
-set RUSTUP_URL=https://win.rustup.rs/rustup-init.exe
-echo Downloading %RUSTUP_URL%...
-powershell -Command "(New-Object Net.WebClient).DownloadFile('%RUSTUP_URL%', 'rustup-init.exe')"
-if %ERRORLEVEL% NEQ 0 (
-  echo ...downloading rustup failed.
-  exit 1
-)
-rustup-init.exe -yv --default-toolchain %channel% --default-host %target%
-if %ERRORLEVEL% NEQ 0 exit 1
+appveyor DownloadFile https://win.rustup.rs/ -FileName rustup-init.exe
+rustup-init -yv --default-toolchain %channel% --default-host %target%
+set PATH=%PATH%;%USERPROFILE%\.cargo\bin
 rustc -vV
 cargo -vV
 
 rem Set the paths appropriately
 PATH C:\msys64\%MSYSTEM%\bin;C:\msys64\usr\bin;%PATH%
-PATH %PATH%;%USERPROFILE%\.cargo\bin
 
 rem Upgrade the MSYS2 platform
 bash -lc "pacman --noconfirm --sync --refresh --refresh pacman"
